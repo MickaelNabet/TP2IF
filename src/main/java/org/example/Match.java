f484e6363dac4eb423017adf3b94b250afbc71bd
Mickaël Nabet <mickael.nabet@dauphine.eu>
2022-11-08 19:18:31 +0100

    Tp2 goalsnumber

--- src/main/java/org/example/Match.java
+++ src/main/java/org/example/Match.java
@@ -3,6 +3,7 @@ package org.example;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+
 public class Match {
     public static void getLastRateBySourceAndDest() {
         String url = "https://jsonmock.hackerrank.com/api/football_matches?year=2011&team1=Barcelona&page=1";
@@ -12,11 +13,17 @@ public class Match {
         System.out.println(res);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         getLastRateBySourceAndDest();
+        goalsNumber("Barcelona", 2012);
     }
 
-    public static int goalsNumber(String team , int year){
-
+    public static void goalsNumber(String team, int year) {
+        String url = "https://jsonmock.hackerrank.com/api/football_matches?year="+year+"&team1="+team+"&page=1";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Object> response = restTemplate.getForEntity(url, Object.class);
+        Object res = response.getBody();
+        System.out.println(res);
     }
+
 }
